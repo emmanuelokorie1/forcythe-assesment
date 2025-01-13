edf23a59a911fb6a4dcfe2e272459ea3fb435d5f
Okorie Emmanuel <Emmzzy@MacBook-Pro-2.local>
2025-01-13 17:06:01 +0100

    update: next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode:false,
+  eslint:{
+    ignoreDuringBuilds:false
+  },
+  typescript:{
+    ignoreBuildErrors:false
+  }
 };
 
 export default nextConfig;
